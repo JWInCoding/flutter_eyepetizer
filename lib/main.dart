5868d29464e8bc1275e5e3c204e4f4ef47dcca40
weakself <weakself>
2025-03-18 09:28:07 +0800

    增加双端启动页

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  Future.delayed(Duration(seconds: 2), () {
+    FlutterNativeSplash.remove();
+  });
+
   runApp(const MyApp());
 }
 
