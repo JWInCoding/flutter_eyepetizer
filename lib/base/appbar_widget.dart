1713d0757b77edb1f3b88cbd415f7f8ba1679136
weakself <weakself>
2025-03-21 12:17:51 +0800

    优化统一的 appbar

--- lib/base/appbar_widget.dart
+++ lib/base/appbar_widget.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-appBar(
+AppBar appBar(
   BuildContext context,
   String title, {
   bool showBack = true,
   List<Widget>? actions,
 }) {
   final appBarTheme = Theme.of(context).appBarTheme;
+  final brightness = Theme.of(context).brightness;
+
+  // 根据主题动态设置状态栏样式
+  final SystemUiOverlayStyle systemUiOverlayStyle =
+      brightness == Brightness.light
+          ? const SystemUiOverlayStyle(
+            // 浅色主题
+            statusBarIconBrightness: Brightness.dark, // 状态栏图标黑色
+            statusBarBrightness: Brightness.light, // iOS状态栏背景浅色
+            statusBarColor: Colors.transparent, // Android状态栏透明
+          )
+          : const SystemUiOverlayStyle(
+            // 深色主题
+            statusBarIconBrightness: Brightness.light, // 状态栏图标白色
+            statusBarBrightness: Brightness.dark, // iOS状态栏背景深色
+            statusBarColor: Colors.transparent, // Android状态栏透明
+          );
+
   return AppBar(
     title: Text(
       title,
@@ -17,7 +35,7 @@ appBar(
         fontWeight: FontWeight.bold,
       ),
     ),
-    systemOverlayStyle: SystemUiOverlayStyle(),
+    systemOverlayStyle: systemUiOverlayStyle,
     centerTitle: true,
     elevation: 0,
     backgroundColor: appBarTheme.backgroundColor,
